350ab7639de722b60aa380e8ecd19aa4cb9ea676
Degenerate1456 <186584961+Huuujji@users.noreply.github.com>
2025-10-22 15:02:09 +0800

    Fixed Patient Records now can  show All the existing patients

--- docucare-appointment-management-system/docucare-appointment-management-system/UcPatientRecords.Designer.vb
+++ docucare-appointment-management-system/docucare-appointment-management-system/UcPatientRecords.Designer.vb
@@ -41,26 +41,29 @@ Partial Class UcPatientRecords
         Me.Panel1.Controls.Add(Me.NavbarMenu)
         Me.Panel1.Controls.Add(Me.PictureBox3)
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(784, 38)
+        Me.Panel1.Size = New System.Drawing.Size(1045, 47)
         Me.Panel1.TabIndex = 6
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(332, 12)
+        Me.Label1.Location = New System.Drawing.Point(443, 15)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(123, 20)
+        Me.Label1.Size = New System.Drawing.Size(149, 25)
         Me.Label1.TabIndex = 11
         Me.Label1.Text = "Patient Records"
         '
         'NavbarMenu
         '
         Me.NavbarMenu.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.NavbarMenu.Location = New System.Drawing.Point(700, 3)
+        Me.NavbarMenu.Location = New System.Drawing.Point(933, 4)
+        Me.NavbarMenu.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.NavbarMenu.Name = "NavbarMenu"
-        Me.NavbarMenu.Size = New System.Drawing.Size(75, 29)
+        Me.NavbarMenu.Size = New System.Drawing.Size(100, 36)
         Me.NavbarMenu.TabIndex = 6
         Me.NavbarMenu.Text = "Menu"
         Me.NavbarMenu.UseVisualStyleBackColor = True
@@ -71,17 +74,19 @@ Partial Class UcPatientRecords
         Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.PictureBox3.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(100, 35)
+        Me.PictureBox3.Size = New System.Drawing.Size(133, 43)
         Me.PictureBox3.TabIndex = 6
         Me.PictureBox3.TabStop = False
         '
         'addPatientBtn
         '
         Me.addPatientBtn.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.addPatientBtn.Location = New System.Drawing.Point(3, 44)
+        Me.addPatientBtn.Location = New System.Drawing.Point(4, 54)
+        Me.addPatientBtn.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.addPatientBtn.Name = "addPatientBtn"
-        Me.addPatientBtn.Size = New System.Drawing.Size(160, 29)
+        Me.addPatientBtn.Size = New System.Drawing.Size(213, 36)
         Me.addPatientBtn.TabIndex = 14
         Me.addPatientBtn.Text = "Add New Patient"
         Me.addPatientBtn.UseVisualStyleBackColor = True
@@ -91,33 +96,41 @@ Partial Class UcPatientRecords
         Me.PatientInfoPanel.AutoScroll = True
         Me.PatientInfoPanel.BackColor = System.Drawing.Color.WhiteSmoke
         Me.PatientInfoPanel.Controls.Add(Me.patientLayout)
-        Me.PatientInfoPanel.Location = New System.Drawing.Point(0, 79)
+        Me.PatientInfoPanel.Location = New System.Drawing.Point(0, 97)
+        Me.PatientInfoPanel.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.PatientInfoPanel.Name = "PatientInfoPanel"
-        Me.PatientInfoPanel.Size = New System.Drawing.Size(784, 418)
+        Me.PatientInfoPanel.Size = New System.Drawing.Size(1045, 514)
         Me.PatientInfoPanel.TabIndex = 13
         '
         'patientLayout
         '
-        Me.patientLayout.Location = New System.Drawing.Point(18, 19)
+        Me.patientLayout.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.patientLayout.AutoScroll = True
+        Me.patientLayout.AutoSize = True
+        Me.patientLayout.Location = New System.Drawing.Point(24, 23)
+        Me.patientLayout.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.patientLayout.Name = "patientLayout"
-        Me.patientLayout.Size = New System.Drawing.Size(743, 370)
+        Me.patientLayout.Size = New System.Drawing.Size(991, 455)
         Me.patientLayout.TabIndex = 0
         '
         'UcPatientRecords
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.docucare_appointment_management_system.My.Resources.Resources.bg_alt
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.Controls.Add(Me.addPatientBtn)
         Me.Controls.Add(Me.PatientInfoPanel)
         Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "UcPatientRecords"
-        Me.Size = New System.Drawing.Size(784, 501)
+        Me.Size = New System.Drawing.Size(1045, 617)
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PatientInfoPanel.ResumeLayout(False)
+        Me.PatientInfoPanel.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
